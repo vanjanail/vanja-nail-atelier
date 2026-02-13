150602dd6528f0e10856008b8f6bd5bfa5bfe6fb
Vanja <vanja.nail.atelier@gmail.com>
2026-02-13 19:19:42 +0100

    wartung_alle Subseiten umleiten

--- .wrangler/tmp/dev-2QwUdN/pages-shim.js
+++ .wrangler/tmp/dev-2QwUdN/pages-shim.js
@@ -50,7 +50,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-LVf7vq/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-GRVSpa/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -82,7 +82,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-LVf7vq/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-GRVSpa/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
